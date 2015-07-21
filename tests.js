0cd07587df533405922a2c4c21c47c454c7029e8
Sergey Romanov <xxsmotur@gmail.com>
2015-07-21 22:57:00 +0500

    Add tests

---
+++ tests.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var appro = require('./lib/appro');
+
+var items = ['time', 'just', 'know', 'back', 'move', 'day', 'work', 'give', 'word', 'last', 'past', 'noise', 'check', 'good', 'look'];
+
+describe('test results based on ngrams', function(){
+    var model = new appro.Appro(items);
+    it('should return "good"', function(){
+        assert.deepEqual(model.ngrams('mood'), ['good']);
+    });
+
+    it('should return empty list', function(){
+        assert.deepEqual(model.ngrams('change'), []);
+    });
+
+    it('bgrams. it should return two words', function(){
+        assert.deepEqual(model.ngrams('world',n=2), ['work', 'word']);
+    });
+});
+
+
+describe('test results based on Levenshtein distance', function(){
+    var model = new appro.Appro(items);
+    it('should return correct word', function(){
+        assert.deepEqual(model.fuzzy('bock'), ['back']);
+    });
+});
